30db02d59ca159fd73583d61f58b35d763923eeb
LeutrimMorina13 <leutrim.morina13@student.uni-pr.edu>
2024-04-21 19:54:31 +0200

    Update Polibius_Cipher.java

--- Polibius/Polibius_Cipher.java
+++ Polibius/Polibius_Cipher.java
@@ -3,21 +3,24 @@ import java.util.Scanner;
 
 public class Polibius_Cipher {
     public static void main(String[] args) {
-        Scanner scanner= new Scanner(System.in);
-        String x = " ", y;
+        Scanner scanner = new Scanner(System.in);
+        String x = "", y;
         System.out.println("Do you want to encrypt or decrypt ? E / D");
-        y=scanner.next();
-        while(!y.equals("E") && !y.equals("D")){
-            System.out.println("Wrong choice!");
-            System.out.println("Please choose again: ");
-            y=scanner.next();
+        y = scanner.next();
+        while (!y.equals("E") && !y.equals("D")) {
+            System.out.println("Wrong choice");
+            System.out.print("Please enter again: ");
+            y = scanner.next();
         }
         char key;
-        for(int i=0; i < 5; i++){
-            System.out.println("Enter the key please: ");
+
+        for (int i = 0; i < 5; i++) {
+            System.out.print("Enter the key, it can be letters or numbers: ");
             key = scanner.next().charAt(0);
-            x +=key;
+            x += key;
         }
+        /*Deklarimi i nje matrice 6x6 e cila kolonen e pare dhe rreshtin
+         *e pare e ka te rezervuar per celesin qe e jep useri*/
         char[][] table = {
                 {' ', x.charAt(0), x.charAt(1), x.charAt(2), x.charAt(3), x.charAt(4)},
                 {x.charAt(0), 'A', 'B', 'C', 'D', 'E'},
@@ -26,20 +29,21 @@ public class Polibius_Cipher {
                 {x.charAt(3), 'Q', 'R', 'S', 'T', 'U'},
                 {x.charAt(4), 'V', 'W', 'X', 'Y', 'Z'}
         };
-        for(int i=0;i< 6; i++){
-            for(int j=0; j< 6; j++){
-                System.out.print(table[i][j]+ " ");
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 6; j++) {
+                System.out.print(table[i][j] + " ");
             }
             System.out.println();
         }
-        if(y.equals("E")){
+        if (y.equals("E")) {
             String plain_text, cipher = "";
             int len;
-            System.out.println("Please enter the plain text: ");
+            System.out.print("Enter the plain text: ");
             scanner.nextLine();
-            plain_text=scanner.nextLine();
+            plain_text = scanner.nextLine();
             len = plain_text.length();
-            plain_text=plain_text.toUpperCase();
+            plain_text = plain_text.toUpperCase();
+
             for(int n=0;n<len;n++){
                 for(int i = 0; i < 6; i++){
                     for (int j = 0; j < 6; j++){
